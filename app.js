8148685fd583990ce3e52ca95f892f210684d7cd
chaima-tn <chaima.bouaziz@insat.u-carthage.tn>
2021-05-03 17:06:41 -0700

    Fixes to ProductController : 	- Protecting paths [createdAt , orders , imgPath] . 	- Uploading image only if the product is added to DB succesfully . 	- Removing a product removes it from the list of the products of 	  its store from the DB .

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ async function start() {
 
     //CORS specification .
     app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*"); //Allowing cross origin access froma any origin.
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
